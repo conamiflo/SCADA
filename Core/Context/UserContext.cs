1d3fd431029c07dc525d3bc823a577a768391059
Stefan Djurica <104830728+StefanDjuricaSV35-2021@users.noreply.github.com>
2024-06-15 22:21:01 +0200

    Added user class and some nuget packages

---
+++ Core/Context/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Model;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Core.Context
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Core/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Core.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public User() { }
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
