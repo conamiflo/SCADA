d0058af180233dc5f44c3e466d769ac4bfe3608e
Nemanja Stjepanović <nemanjastjepanovic222@gmail.com>
2024-06-15 19:54:41 +0200

    initial commit

---
+++ Core/Model/Tag/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Core.Model.Tag
+{
+    public abstract class Tag
+    {
+        string TagName { get; set; }
+        string Description { get; set; }
+        string IOAddress { get; set; }
+    }
+}
