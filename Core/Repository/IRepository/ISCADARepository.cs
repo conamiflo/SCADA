eeff6b97fbb7307ed95b6ba58d8a4b82be935ef0
Nesa02 <nenad.beric00@gmail.com>
2024-06-18 18:01:09 +0200

    added scada repository interface

---
+++ Core/Repository/IRepository/ISCADARepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repository.IRepository
+{
+    internal interface ISCADARepository
+    {
+        AnalogInput GetAnalogInput(string id);
+        AnalogInput AddAnalogInput(AnalogInput analogInput);
+        bool DeleteAnalogInput(string id);
+        AnalogInput UpdateAnalogInput(AnalogInput analogInput);
+
+    }
+}
