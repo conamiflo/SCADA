5dbfc08c626459612286b91fa0c5e0cc2b929dc7
Borislav Čelar <bora.celar@gmail.com>
2024-06-19 13:20:11 +0200

    digital tag creation, alarm creation

--- Core/Model/Tag/DigitalInput.cs
+++ Core/Model/Tag/DigitalInput.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿using Core.Model.Alarm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Core.Model.Tag
 {
-    public class DigitalInput : Tag
+    public class DigitalInput : Tag,IInputTag
     {
+        public DigitalInput(string tagName, string description, string iOAddress,string driver, double scanTime, bool isOn) : base(tagName, description, iOAddress)
+        {
+            ScanTime = scanTime;
+            IsOn = isOn;   
+        }
+
         public double ScanTime { get; set; }
         public bool IsOn { get; set; }
+
+        public void addAlarm(Alarm.Alarm alarm)
+        {
+            Alarms.Add(alarm);
+        }
     }
+
+    
 }
