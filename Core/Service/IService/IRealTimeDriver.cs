66a2d26a25d670a9b1c8d58a6798aa08eedb8dde
Nemanja Stjepanović <nemanjastjepanovic222@gmail.com>
2024-06-20 15:52:39 +0200

    c

--- Core/Service/IService/IRealTimeDriver.cs
+++ Core/Service/IService/IRealTimeDriver.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Driver
+namespace Core.Service.IService
 {
     [ServiceContract]
     public interface IRealTimeDriver
--- Core/Service/RealTimeDriver.cs
+++ Core/Service/RealTimeDriver.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Core.Driver
+namespace Core.Service
 {
     public class RealTimeDriver : IRealTimeDriver
     {
