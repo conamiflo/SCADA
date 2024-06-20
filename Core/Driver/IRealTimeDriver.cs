197aa3f2f5c00a2adfba5e27bfbb5f9a41a4b0d5
Nemanja Stjepanović <nemanjastjepanovic222@gmail.com>
2024-06-20 15:50:56 +0200

    fixed folders

--- Core/Driver/IRealTimeDriver.cs
+++ Core/Driver/IRealTimeDriver.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.RealTimeDriver
+namespace Core.Driver
 {
     [ServiceContract]
     public interface IRealTimeDriver
--- Core/Driver/RealTimeDriver.cs
+++ Core/Driver/RealTimeDriver.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Core.RealTimeDriver
+namespace Core.Driver
 {
     public class RealTimeDriver : IRealTimeDriver
     {
